a65a27b3e163d2a02a7de3cec5486fc53581f798
geekysky <debu69980@gmail.com>
2026-04-24 13:35:25 +0530

    WebSocket Fundamentals Setup

---
+++ webSocket/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import http from "http";
+import path from "path";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Socket.io connections 
+
+
+
+
+app.use(express.static(path.resolve('./public')));
+
+app.get('/', (req,res)=> {
+    return res.sendFile('/public/index.html');
+});
+
+server.listen(9000, ()=>{
+    console.log(`Server Started at Port:9000`);
+});
