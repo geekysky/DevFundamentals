0fee1abe4b0e619cee9afbcd07accc42d7b8c87a
geekysky <debu69980@gmail.com>
2026-05-04 22:59:05 +0530

    Add Redis list operations and modify string handling

---
+++ Redis/list.js
@@ -0,0 +1,14 @@
+import client from "./client.js";
+
+async function init(){
+    await client.lpush('messages',1);
+    await client.lpush('messages',2);
+    await client.lpush('messages',3);
+    await client.lpush('messages',4);
+
+    const result = await client.lpop('messages');
+    console.log(result);
+
+}
+
+init();
--- Redis/string.js
+++ Redis/string.js
@@ -2,6 +2,8 @@ import client from "./client.js";
 
 async function init(){
     await client.set("msg:6","Hello from Node.js");
+
+    //await client.expire("msg:6",10);
     const result = await client.get('msg:6');
     console.log(result);
 }
